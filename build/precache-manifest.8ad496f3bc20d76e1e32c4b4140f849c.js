b61346bc0a3bfe4c10b26d852ce53e4ebcf8ce9d
samy-ard <sami.david.ardelean@gmail.com>
2025-01-31 14:46:24 +0200

    Deploy 2

---
+++ build/precache-manifest.8ad496f3bc20d76e1e32c4b4140f849c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10e7e253d2f4ea756fbbe715f9a2823f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b7fe24a5d61aea788d8",
+    "url": "/static/css/main.b841a812.chunk.css"
+  },
+  {
+    "revision": "8af52551afb6189bb900",
+    "url": "/static/js/2.457c38c8.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.457c38c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b7fe24a5d61aea788d8",
+    "url": "/static/js/main.e015150c.chunk.js"
+  },
+  {
+    "revision": "1e4aacf39f7b6bf0231c",
+    "url": "/static/js/runtime-main.02538176.js"
+  },
+  {
+    "revision": "2dd767322ccc760c0c31d6a900b9d3b2",
+    "url": "/static/media/DMSans-Medium.2dd76732.ttf"
+  },
+  {
+    "revision": "eb99628e146d75ddc65f05f0fee6e5a1",
+    "url": "/static/media/DMSans-MediumItalic.eb99628e.ttf"
+  },
+  {
+    "revision": "916f3ae325c2bcd71bec9b469c6a6aa0",
+    "url": "/static/media/DMSans-Regular.916f3ae3.ttf"
+  }
+]);
